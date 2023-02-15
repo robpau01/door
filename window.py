fa20d74f0fca3da10f3d53c1359e08a6f6816050
Robert Paulsson <85135830+robpau01@users.noreply.github.com>
2023-02-15 17:21:59 +0100

    Create window.py

---
+++ window.py
@@ -0,0 +1,38 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+            
+@app.route('/')
+def window():
+                xml = '''<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+                    <window>    
+                        <items>
+                            <item id=\"234738947\">
+                                <id>234738947</id>
+                                <details>
+                                    <material>Glas</material>
+                                    <style>Toned</style>
+                                    <size>15x65</size>
+                                </details>
+                            </item>
+                            <item id=\"2344568947\">
+                                <id>2344568947</id>
+                                <details>
+                                    <material>Plastic</material>
+                                    <style>None</style>
+                                    <size>15x65</size>
+                                </details>
+                            </item>
+                            <item id=\"235435\">
+                                <id>235435</id>
+                                <details>
+                                    <material>Plywood</material>
+                                    <style>Blacked</style>
+                                    <size>15x64</size>
+                                </details>
+                            </item>
+                        </items>
+                    <copyright>2002 - Martin</copyright>
+                    </window>'''
+return Response(xml, mimetype='xml')"
+}
