56de2a306a193788d0f696427e1899116fc1ec33
Robert Paulsson <85135830+robpau01@users.noreply.github.com>
2023-03-02 16:40:12 +0100

    Create app.py

---
+++ book/app.py
@@ -0,0 +1,31 @@
+
+import os
+import psycopg2
+from flask import Flask, render_template, request, url_for, redirect
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+               host="127.0.0.1",
+               port="8989",
+               database="userdb1",
+               user="postgres",
+               password="Passw0rd")
+    return conn
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM books;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template('index.html', books=books)
+
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    return render_template('create.html')
